0a917d59bfd8922f79cd1f653312b920fc2b3d28
김준영 <kjy1787@newlinkcorp.co.kr>
2022-10-25 17:45:14 +0900

    질문리스트 수정

--- public/static/questionList.js
+++ public/static/questionList.js
@@ -2,10 +2,12 @@ const inerview = {};
 
 inerview.cs = [
   {
+    importance: "5",
     question: "객체 지향 프로그래밍에 대해 설명해주세요.",
     answer: "객체지향이란",
   },
   {
+    importance: "5",
     question: "Process와 Thread의 차이에 대해 설명해주세요.",
     answer: "Process와 Thread는...",
   },
@@ -13,10 +15,12 @@ inerview.cs = [
 
 inerview.javascript = [
   {
+    importance: "5",
     question: "sync와 async의 차이점을 설명해주세요.",
     answer: "동기 비동기는...",
   },
   {
+    importance: "5",
     question: "blocking과 non-blocking의 차이를 설명해주세요.",
     answer: "동기 비동기는...",
   },
