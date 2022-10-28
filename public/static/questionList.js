4bb537e910e0e7bf066f7daaa0da89e344deadd1
김준영 <kjy1787@newlinkcorp.co.kr>
2022-10-28 11:41:28 +0900

    질문 리스트 추가

--- public/static/questionList.js
+++ public/static/questionList.js
@@ -1,29 +1,298 @@
 const inerview = {};
 
-inerview.cs = [
+inerview.CS = [
   {
     importance: "5",
     question: "객체 지향 프로그래밍에 대해 설명해주세요.",
-    answer:
-      "객체지향이란@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
+    answer: "",
   },
   {
     importance: "5",
     question: "Process와 Thread의 차이에 대해 설명해주세요.",
-    answer: "Process와 Thread는...",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "Stack과 Queue의 차이에 대해 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "Map과 Set의 차이점을 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "라이브러리와 프레임워크의 차이점을 설명해주세요.",
+    answer: "",
   },
 ];
 
-inerview.javascript = [
+inerview.JavaScript = [
   {
     importance: "5",
     question: "sync와 async의 차이점을 설명해주세요.",
-    answer: "동기 비동기는...",
+    answer: "",
   },
   {
     importance: "5",
     question: "blocking과 non-blocking의 차이를 설명해주세요.",
-    answer: "동기 비동기는...",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "콜백 함수에 대해 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "콜백 지옥을 해결하는 방법을 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "promise와 async, await의 차이를 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "var, let, const의 차이를 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "함수 선언형과 함수 표현식의 차이를 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "호이스팅에 대해 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "이벤트 버블링과 이벤트 캡처링에 대해 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "클로져에 대해 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "렉시컬 환경에 대해 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "실행 컨텍스트에 대해 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "프로토 타입에 대해 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "깊은복사와 얕은복사의 차이점을 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "this의 의미를 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "ES6에서 생긴 큰 변화들에 대해 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "스코프와 스코프체인에 대해 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "import와 require의 차이점에 대해 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "package.json의 역할에 대해 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "typescript를 쓰는 이유에 대해 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "null, undefined, undeclared, NaN에 대해 설명해주세요.",
+    answer: "",
+  },
+];
+
+inerview.React = [
+  {
+    importance: "5",
+    question: "Virtual DOM에 대해 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "React의 장점과 단점을 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "State와 Props의 차이를 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "React lifecycle에 대해 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question:
+      "state를 직접변경하지 않고 useState를 사용하는 이유를 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "useEffect에 대해 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "useCallback과 useMemo의 차이를 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "React rendering 성능을 향상시키기 위한 방법들을 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "전역 상태 관리 방법에 대해 설명해주세요.",
+    answer: "",
+  },
+];
+
+inerview.Frontend = [
+  {
+    importance: "5",
+    question: "브라우저 주소창에 google을 입력하면 발생하는 일을 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "DNS에 대해 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "REST API에 대해 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "브라우저 렌더링 과정을 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "Webpack, Babel, Polyfill에 대해 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "SPA, CSR, SSR의 차이를 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "CORS가 무엇인지, 해결해 본 경험이 있으신지 말씀해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "Sementic Markup에 대해 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "로컬 스토리지, 세션 스토리지, 쿠키에 대해 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "Event loop와 Task queue에 대해 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "MVC, MVVM 패턴에 대해 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "HTTP와 HTTPS의 차이점을 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "페이지 로드 시간을 줄이는 방법들에 대해서 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "테스트 코드를 작성하신 경험이 있으신가요?",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "웹 서비스 배포 시스템 구축 경험이 있으신가요?",
+    answer: "",
+  },
+];
+
+inerview.etc = [
+  {
+    importance: "5",
+    question: "자기소개 부탁드립니다.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "회사 지원 동기가 어떻게 되시나요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "최근에 경험했던 기억에 남는 에러에 대해 말씀해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "요즘 공부중이시는 것에대해 설명해주세요.",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "새로운 기술을 습득하기 위해 어떠한 방식으로 연구하시나요?",
+    answer: "",
+  },
+  {
+    importance: "5",
+    question: "협업을 하면서 갈등이 생겼던 경험이 있으신가요?",
+    answer: "",
   },
 ];
 
