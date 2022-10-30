4de27eea027c036e6bb4736538e43c0edace0768
ma9pie <ma9pie.dev@gmail.com>
2022-10-30 21:29:49 +0900

    배포 테스트

--- components/common/ScrollBar.jsx
+++ components/common/ScrollBar.jsx
@@ -2,32 +2,7 @@ import styled from "@emotion/styled";
 import React, { useCallback, useEffect, useRef } from "react";
 import SimpleBar from "simplebar-react";
 
-function ScrollBar({
-  width,
-  height,
-  maxHeight,
-  setHeight,
-  targetRef,
-  children,
-}) {
-  // 창 크기
-  const resizeListener = useCallback(() => {
-    setHeight(targetRef?.current?.offsetHeight);
-  }, []);
-
-  // resize 리스너 실행
-  useEffect(() => {
-    resizeListener();
-  }, [resizeListener]);
-
-  // resize 이벤트
-  useEffect(() => {
-    window.addEventListener("resize", resizeListener, { passive: true });
-    return () => {
-      window.removeEventListener("resize", resizeListener);
-    };
-  });
-
+function ScrollBar({ width, height, maxHeight, children }) {
   return <SimpleBar style={{ width, height, maxHeight }}>{children}</SimpleBar>;
 }
 
@@ -36,5 +11,4 @@ export default ScrollBar;
 ScrollBar.defaultProps = {
   width: "100%",
   height: "auto",
-  setHeight: () => {},
 };
