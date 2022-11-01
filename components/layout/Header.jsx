ecbbdad8ad5c18c5b44302d58660d1b1d3ea55a0
ma9pie <ma9pie.dev@gmail.com>
2022-11-01 23:50:39 +0900

    레이아웃 테스트

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -23,11 +23,9 @@ function Header(props) {
 export default Header;
 
 const Wrapper = styled.header`
-  position: absolute;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  top: 0px;
   width: 100%;
   height: 60px;
   padding: 0px 20px;
--- layouts/CommonLayout.jsx
+++ layouts/CommonLayout.jsx
@@ -7,23 +7,20 @@ import Header from "@/components/layout/Header";
 
 function CommonLayout(props) {
   return (
-    <Container>
-      <Head></Head>
-      <Header></Header>
-      <ScrollBar height="calc(100vh - 60px)">
+    <ScrollBar height="100vh">
+      <Container>
+        <Head></Head>
+        <Header></Header>
         <Body>{props.children} </Body>
         <Footer></Footer>
-      </ScrollBar>
-    </Container>
+      </Container>
+    </ScrollBar>
   );
 }
 
 export default CommonLayout;
 
-const Container = styled.div`
-  position: relative;
-  padding-top: 60px;
-`;
+const Container = styled.div``;
 const Body = styled.div`
   min-height: calc(100vh - 100px);
 `;
