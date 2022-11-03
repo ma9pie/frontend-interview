51db298d130d91ade8180919f87a9e4e57a1e500
ma9pie <ma9pie.dev@gmail.com>
2022-11-03 22:31:10 +0900

    레이아웃 테스트

--- layouts/CommonLayout.jsx
+++ layouts/CommonLayout.jsx
@@ -1,13 +1,17 @@
 import styled from "@emotion/styled";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ScrollBar from "@/components/common/ScrollBar";
 import Footer from "@/components/layout/Footer";
 import Head from "@/components/layout/Head";
 import Header from "@/components/layout/Header";
 
 function CommonLayout(props) {
+  const scrollInto = useRef(null);
+  useEffect(() => {
+    scrollInto.current.scrollIntoView();
+  });
   return (
-    <Container>
+    <Container ref={scrollInto}>
       <Head></Head>
       <Header></Header>
       <Body>
