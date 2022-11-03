09458af4271aa252dbe60dcaab8917f4f992a3ed
ma9pie <ma9pie.dev@gmail.com>
2022-11-03 23:36:51 +0900

    구글 검색 메타태그 추가

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,10 @@ export default function Document() {
       <Head>
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="google-site-verification"
+          content="0ATr3A6QdCZG5gVNu2lZep3W5w7d3aKLynPPdcKH370"
+        />
         <link rel="icon" href="/public/favicon.ico" />
         {/* 다크모드 시 화면 깜빡임 제거 */}
         <script
