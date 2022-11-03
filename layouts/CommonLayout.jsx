0dc203ac5dc504419242dad7e1c1ef4c8371147d
ma9pie <ma9pie.dev@gmail.com>
2022-11-03 22:50:43 +0900

    레이아웃 테스트

--- layouts/CommonLayout.jsx
+++ layouts/CommonLayout.jsx
@@ -6,13 +6,28 @@ import Head from "@/components/layout/Head";
 import Header from "@/components/layout/Header";
 
 function CommonLayout(props) {
-  useEffect(() => {}, []);
+  const [innerHeight, setinnerHeight] = useState(1000);
+
+  useEffect(() => {
+    setHeight();
+    window.addEventListener("resize", setHeight);
+    return () => {
+      window.removeEventListener("resize", setHeight);
+    };
+  });
+
+  const setHeight = () => {
+    setinnerHeight(window.innerHeight);
+  };
+
   return (
     <Container>
       <Head></Head>
       <Header></Header>
       <Body>
-        <ScrollBar maxHeight="calc(100vh - 100px)">{props.children}</ScrollBar>
+        <ScrollBar maxHeight={`${innerHeight - 100}px`}>
+          {props.children}
+        </ScrollBar>
       </Body>
       <Footer></Footer>
     </Container>
