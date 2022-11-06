5a5b62ad196c4c024c5e026c0f80682c43c78fff
ma9pie <ma9pie.dev@gmail.com>
2022-11-06 23:08:16 +0900

    구글 애널리틱스 유틸 수정

--- utils/GoogleAnalyticsUtils.js
+++ utils/GoogleAnalyticsUtils.js
@@ -1,7 +1,7 @@
 const GoogleAnalyticsUtils = () => {};
 
 GoogleAnalyticsUtils.changeRouteGtag = (url) => {
-  window.gtag("config", process.env.NEXT_PUBLIC_GA_ID, {
+  window.gtag("config", process.env.NEXT_PUBLIC_GA_TRACKING_ID, {
     page_path: url,
   });
 };
