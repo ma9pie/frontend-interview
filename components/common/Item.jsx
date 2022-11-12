fbe18bce59404b438f6e1e7c4c178e3af19526f0
김준영 <kjy1787@newlinkcorp>
2022-11-12 17:31:39 +0900

    react-collapse 제거 및 네이티브 개발

--- components/common/Item.jsx
+++ components/common/Item.jsx
@@ -1,42 +1,49 @@
 import { css } from "@emotion/css";
 import styled from "@emotion/styled";
 import React, { useRef, useState } from "react";
-import { Collapse } from "react-collapse";
 import Answer from "@/components/common/Answer";
 import Question from "@/components/common/Question";
 
 const Item = (props) => {
+  const ref = useRef();
+  const [height, setHeight] = useState("auto");
   const [isOpenAnswer, setIsOpenAnswer] = useState(true);
+
+  const collapse = () => {
+    const target = ref.current;
+    const { clientHeight, scrollHeight } = target;
+    if (isOpenAnswer) {
+      setIsOpenAnswer(false);
+      setHeight(`${scrollHeight}px`);
+      setTimeout(() => {
+        setHeight("0px");
+      }, 0);
+    } else {
+      setIsOpenAnswer(true);
+      setHeight("0px");
+      setTimeout(() => {
+        setHeight(`${scrollHeight}px`);
+      }, 0);
+    }
+  };
 
   return (
     <Wrapper>
-      <QuestionWrapper onClick={() => setIsOpenAnswer(!isOpenAnswer)}>
+      <QuestionWrapper onClick={collapse}>
         <Question
           isOpenAnswer={isOpenAnswer}
           question={props.question}
           answer={props.answer}
         ></Question>
       </QuestionWrapper>
-      <Collapse
-        isOpened={isOpenAnswer}
-        initialStyle={{ height: 0, overflow: "hidden" }}
-      >
-        <AnswerWrapper className={isOpenAnswer ? Open : Close}>
-          <Answer answer={props.answer}></Answer>
-        </AnswerWrapper>
-      </Collapse>
+      <AnswerWrapper ref={ref} height={height}>
+        <Answer answer={props.answer}></Answer>
+      </AnswerWrapper>
     </Wrapper>
   );
 };
 
 export default Item;
-
-const Open = css`
-  opacity: 1;
-`;
-const Close = css`
-  opacity: 0;
-`;
 
 const Wrapper = styled.div`
   margin-bottom: 10px;
@@ -45,6 +52,8 @@ const QuestionWrapper = styled.span`
   cursor: pointer;
 `;
 const AnswerWrapper = styled.div`
-  padding: 8px;
-  transition: opacity 0.5s ease;
+  margin: 8px;
+  transition: all 0.3s ease;
+  overflow: hidden;
+  height: ${(props) => props.height}; ;
 `;
