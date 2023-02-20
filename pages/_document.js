d79a62597307e6020ae2b8988e997a8447bcbb83
ma9pie <ma9pie.dev@gmail.com>
2023-02-21 01:29:14 +0900

    SEO 최적화를 위한 메타태그 설정

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,48 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta charset="utf-8" />
+        <meta name="robots" content="index,nofollow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <meta name="title" content="프론트엔드 기술 면접 질문" />
+        <meta
+          name="description"
+          content="구글, 티스토리, 깃헙 등등 프론트엔드(Frontend) 기술 면접에서 자주 물어보는 질문 자료들을 정리하여 만든 사이트입니다."
+        />
+        <meta
+          name="keywords"
+          content="HTML, CSS, JavaScript, React, Frontend, Interview"
+        />
+        <meta name="author" content="ma9pie" />
+
+        {/* Naver 블로그, 카카오톡 미리보기 설정 */}
+        <meta property="og:title" content="프론트엔드 기술 면접 질문" />
+        <meta
+          property="og:url"
+          content="https://frontend-interview-question.vercel.app"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="프론트엔드 기술 면접 질문" />
+        <meta
+          property="og:description"
+          content="구글, 티스토리, 깃헙 등등 프론트엔드(Frontend) 기술 면접에서 자주 물어보는 질문 자료들을 정리하여 만든 사이트입니다."
+        />
+
+        {/* 트위터 미리보기 설정 */}
+        <meta name="twitter:card" content="프론트엔드 기술 면접 질문" />
+        <meta name="twitter:title" content="프론트엔드 기술 면접 질문" />
+        <meta
+          name="twitter:description"
+          content="구글, 티스토리, 깃헙 등등 프론트엔드(Frontend) 기술 면접에서 자주 물어보는 질문 자료들을 정리하여 만든 사이트입니다."
+        />
+
+        {/* 구글 서치 콘솔 */}
+        <meta
+          name="google-site-verification"
+          content="0ATr3A6QdCZG5gVNu2lZep3W5w7d3aKLynPPdcKH370"
+        />
+
         {/* 아이콘 생성 사이트 - https://favicomatic.com */}
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
@@ -32,7 +74,6 @@ export default function Document() {
           name="msapplication-square310x310logo"
           content="icons/mstile-310x310.png"
         />
-
         <link rel="manifest" href="manifest.json" />
         <link
           rel="apple-touch-icon"
@@ -104,11 +145,7 @@ export default function Document() {
           href="icons/favicon-128.png"
           sizes="128x128"
         />
-        {/* 구글 서치 콘솔 */}
-        <meta
-          name="google-site-verification"
-          content="0ATr3A6QdCZG5gVNu2lZep3W5w7d3aKLynPPdcKH370"
-        />
+
         {/* 구글 애널리틱스 */}
         <script
           async
@@ -128,12 +165,14 @@ export default function Document() {
             `,
           }}
         />
+
         {/* 구글 애드센스 */}
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4808054974810104"
           crossOrigin="anonymous"
         ></script>
+
         {/* 다크모드 시 화면 깜빡임 제거 */}
         <script
           dangerouslySetInnerHTML={{
