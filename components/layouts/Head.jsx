f8b5f4393d0d4e5edd6b7d56433573a8f1b0e0d0
ma9pie <ma9pie.dev@gmail.com>
2024-01-07 05:04:23 +0900

    fix : Head meta 수정

--- components/layouts/Head.jsx
+++ components/layouts/Head.jsx
@@ -5,7 +5,10 @@ function HeadComponent({ title = "프론트엔드 면접질문" }) {
   return (
     <Head>
       <title>{title}</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+      ></meta>
     </Head>
   );
 }
